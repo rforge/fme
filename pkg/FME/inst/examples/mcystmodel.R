747550befcb016e530f4b45d0c94e8322cfd3de1
petzoldt <petzoldt@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-03-20 00:12:53 +0000

    cleanup of "todo.txt"; tests with mcystmodel.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/fme@25 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/FME/inst/examples/mcystmodel.R
+++ pkg/FME/inst/examples/mcystmodel.R
@@ -1,16 +1,19 @@
 library("simecol")
 require(FME)
 
+## unit conversion
 fem2mu <- 1e+6 * 1e-15
 mu2fem <- 1e+9
 
+## function that plots simulated and observed data
 plot.mcyst <- function(ex, sc) {
   o <- out(sc)
   nf <- layout(matrix(c(1, 2), 2, 2, byrow=FALSE), c(3, 1), 1)
   opar <- par(mar=c(0,6.1,2,1), pch=16, cex=1, las=1)
   ylim_cells <- c(0, max(o$cells, na.omit(ex$cells)))
   ylim_mcyst  <- c(0, max(o$mcyst*fem2mu, na.omit(ex$mcyst)))
-  plot(ex$time, ex$cells, axes=FALSE, col="forestgreen", ylab="", ylim=ylim_cells)
+  plot(ex$time, ex$cells, axes=FALSE, col="forestgreen", ylab="",
+    ylim=ylim_cells)
   mtext(expression(paste("Cells (",mL^{-1}, ")")),
     side=2, line=4, cex=1.2, las=0)
   axis(2)
@@ -21,7 +24,7 @@ plot.mcyst <- function(ex, sc) {
     ylim=ylim_mcyst)
   mtext(expression(paste("MCYST (",mu~g~mL^{-1}, ")")),
     side=2, line=4, cex=1.2, las=0)
-  lines(o$time, o$mcyst * fem2mu)
+  lines(o$time, o$mcyst)# * fem2mu)
   par(opar)
 }
 
@@ -41,40 +44,40 @@ mcyst2 <- new("odeModel",
   #  mcyst = 2000 * 1e5  # Start-MCYST (fg/L)
   #),
   parms = c(
-  	mu = 0.238,       # 1/d (Lab)
-    K  = 31.5e6,      # Cells per ml
-  	p  = 120,         # fg/cell
-  	dM = 0.04,        # 1/d (Orr & Jones)
-  	cells = 1.4e6,
-  	mcyst = 8.6 * 1e7
+    mu = 0.2,         # 1/d (Lab)
+    K  = 30e6,        # Cells per ml
+    p  = 50,          # fg/cell
+    dM = 0.04,        # 1/d (Orr & Jones)
+    cells = 1.4e6,
+    mcyst = 8.6 * 1e7
   ),
-  #inputs = matrix(data.frame(time=0:100, pH=rep(8.6, 101))),
   times = c(from=0, to=100, by=1),
   solver = "lsoda",
+  ## copy two "parameters" to init; this allows fitting initial values
   initfunc = function(obj) {
-    #print(parms(obj))
     init(obj)[c("cells", "mcyst")] <- parms(obj)[c("cells", "mcyst")]
     obj
   }
 )
 
-# Read the data...
+## Read the data...
 dat <- read.table("expS108.txt", header=TRUE, sep="\t")
 dat$mcyst[1] <- 0 ## replace NA
 
-# Make the data compatible with model...
+## Make the data compatible with model...
 dat$mcyst <- dat$mcyst * mu2fem
 
 
-# Simulate,
+## Simulate
 mc2 <- mcyst2
 sc  <- sim(mc2)
 plot.mcyst(dat, sc)
 
-# Parameters to be fitted...
-whichpar <- c("cells", "mcyst", "p", "mu", "K", "dM")
+## Parameters to be fitted...
+whichpar <- c("p", "mu", "K", "dM")
+#whichpar <- c("cells", "mcyst", "p", "mu", "K", "dM")
 lower    <- c(cells = 1e5, mcyst = 0,   p = 0,   mu = 0.1, K = 10e6, dM = 0)
-upper    <- c(cells = 5e6, mcyst = 5e8, p = 120, mu = 1,   K = 40e6, dM = 1)
+upper    <- c(cells = 5e6, mcyst = 5e8, p = 200, mu = 1,   K = 40e6, dM = 1)
 
 #===========================
 # 1. Fit, using fitOdeModel
@@ -85,60 +88,60 @@ times(sc) <- obstime
 
 optdat <- data.frame(cells=dat$cells, mcyst=dat$mcyst )
 
-# this works...
+## this works...
 Oderes <- fitOdeModel(sc, whichpar=whichpar, obstime, optdat,  method="BFGS",
   debuglevel=0, lower = lower, upper = upper, control=list(trace=TRUE))
 
 Oderes$par
 
-# this does not work...
-Oderes2 <- fitOdeModel(sc, whichpar=whichpar, obstime, optdat,  method="L-BFGS-B",
+# this works too...
+Oderes2 <- fitOdeModel(sc, whichpar=whichpar, obstime, optdat,  method="PORT",
   debuglevel=0, lower = lower, upper = upper, control=list(trace=TRUE))
 
+Oderes$par
+
+## asign parms to the model object and simulate again
 parms(sc)[whichpar] <- Oderes$par
 
-times(sc) <-  c(from=0, to=100, by=1)
+times(sc) <- c(from=0, to=100, by=1)
 sc        <- sim(sc)
 plot.mcyst(dat, sc)
 
-#parms(sc)
-#           mu            K            p           dM
-# 2.386138e-01 3.154855e+07 1.200000e+02 3.987729e-02
-#        cells        mcyst
-# 1.424767e+06 8.627639e+07
+#> parms(sc)
+#          mu            K            p           dM        cells        mcyst
+#2.324523e-01 3.180356e+07 1.255524e+02 4.413899e-02 1.498322e+06 7.332897e+07
+
 
 
 #===========================
 # 2. Fit using other algorithms ...
 #===========================
 # Solver function
-Solver <- function (p)
-{
- parms(sc)[whichpar]<- p
- out(sim(sc))
+Solver <- function (p) {
+ parms(mc2)[whichpar]<- p
+ out(sim(mc2))
 }
 
 # Cost of model versus data
-Cost <- function (p)
-{
+Cost <- function (p) {
  Run <- Solver(p)
  return ( modCost(model=Run,obs=dat,weight="std"))
 }
 
 print(system.time(Fit<-modFit(p=parms(mc2)[whichpar],
-                  f=Cost,lower=lower, upper=upper,method="Port")))
+                  f=Cost,lower=lower[whichpar], upper=upper[whichpar],method="Port")))
 summary(Fit)             # not converged...
 
 print(system.time(Fit<-modFit(p=parms(mc2)[whichpar],
-                  f=Cost,lower=lower, upper=upper,method="L-BFGS-B")))
+                  f=Cost,lower=lower[whichpar], upper=upper[whichpar],method="L-BFGS-B")))
 summary(Fit)             # not converged...
 
 print(system.time(Fit<-modFit(p=parms(mc2)[whichpar],
-                  f=Cost,lower=lower, upper=upper,method="BFGS")))
+                  f=Cost,lower=lower[whichpar], upper=upper[whichpar],method="BFGS")))
 summary(Fit)             # not converged...
 
 FITmrq <- modFit(Cost,parms(mc2)[whichpar],
-                  lower=lower, upper=upper,method="Marq")
+                  lower=lower[whichpar], upper=upper[whichpar],method="Marq")
 summary(FITmrq)
 
 # 2. nls.lm fits the model to the data, using the residuals
@@ -170,11 +173,11 @@ pairs(sF)
 
 collin(sF)
 # small enough value for all parameters
-collin(sF,parset=1:6)
+collin(sF,parset=1:length(whichpar))
 
 # or, equivalently - and quicker!
 sF2 <- sensFun(func=Cost,parms=BestPar)
-collin(sF2,1:6)  # collinearity of all parameters...
+collin(sF2,1:length(whichpar))  # collinearity of all parameters...
 
 #=====================
 # 4. MCMC application
@@ -191,8 +194,8 @@ Covar  <- sP$cov.scaled * (2.4^2)/6
 
 # so, 20% of parameter value is used for initial jump length
 MCMC <- modMCMC(f=Cost,p=FitMrq$par,jump=FitMrq$par*0.2,niter=5000,
-                var0=s2prior,wvar0=1,updatecov=10,lower=c(0,0,0,0,0,0),
-                upper = c(1e8,1e9,500,2,1e9,1))
+                var0=s2prior,wvar0=1,updatecov=10,lower=rep(0, length(whichpar)),
+                upper = c(cells=1e8, mcyst=1e9,p=500,mu=2,K=1e9,dM=1)[whichpar])
 
 plot(MCMC,Full=TRUE)     # not quite converged
 hist(MCMC,Full=TRUE,col="blue")
