50c970f0cd4c4e3a498715852002dae2671c7bd0
petzoldt <petzoldt@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-07-22 21:35:29 +0000

    try to understand
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/fme@51 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/FME/R/collin.R
+++ pkg/FME/R/collin.R
@@ -2,14 +2,14 @@
 ## Collinearity indices
 ## -----------------------------------------------------------------------------
 
-collin <- function(sensfun, parset=NULL, N=NULL, which=NULL) {
+collin <- function(sensfun, parset = NULL, N = NULL, which = NULL) {
 
-  if (is.null(colnames(sensfun)))colnames(sensfun)<-1:ncol(sensfun)
+  if (is.null(colnames(sensfun)))colnames(sensfun) <- 1:ncol(sensfun)
 
   if (!is.null(which)) {
-    nx  <-attr(sensfun,"nx")
-    var <-attr(sensfun,"var")
-    TYP <-attr(sensfun,"Type")
+    nx  <- attr(sensfun, "nx")
+    var <- attr(sensfun, "var")
+    TYP <- attr(sensfun, "Type")
 
     if (! is.numeric(which)) {
       ln <- length(which)
@@ -24,26 +24,24 @@ collin <- function(sensfun, parset=NULL, N=NULL, which=NULL) {
     ii <- NULL
 
     if (TYP == 1)
-     for (i in Select)
-       ii <- c(ii,((i-1)*nx):(i*nx))
-    else  for (i in Select)
-       ii <- c(ii,(nx[i]+1):nx[i+1])
+     for (i in Select)   ii <- c(ii, ((i-1)*nx):(i*nx))
+    else
+      for (i in Select)  ii <- c(ii, (nx[i]+1):nx[i+1])
     sensfun <- sensfun[ii,]
-    }
+  }
 
   if (colnames(sensfun)[1]=="x" && colnames(sensfun)[2] == "var")
     Sens <- sensfun[,-(1:2)]
-  else Sens<-sensfun
-
+  else Sens <- sensfun
 
   npar <- ncol(Sens)
-  L2 <- sqrt(colSums(Sens*Sens))
+  L2   <- sqrt(colSums(Sens*Sens))
 
   iNa <- 0
-  # Check for non-identifiable parameters
-  if ( any (L2 == 0 ) ) {
-    iNa<-which(L2==0)
-    warning (paste("Sensitivity of parameter",colnames(Sens)[iNa],"is 0! "))
+  ## Check for non-identifiable parameters
+  if (any(L2 == 0 ) ) {
+    iNa <- which(L2 == 0)
+    warning (paste("Sensitivity of parameter", colnames(Sens)[iNa], "is 0! "))
   }
   if (npar > 14 & is.null(parset) & is.null(N))
     warning ("will reduce collinearity estimates: too many combinations")
@@ -51,40 +49,42 @@ collin <- function(sensfun, parset=NULL, N=NULL, which=NULL) {
   normSens <- t(t(Sens) / L2)
 
   Collin <- NULL
-  # internal function to generate collinearities
-  # for a given set of parameter combinations
+  ## internal function to generate collinearities
+  ## for a given set of parameter combinations
 
   collFun <- function(cc) {
     for (i in 1:nrow(cc)) {
       ii    <- cc[i,]
       S     <- normSens[,ii]
       Nident <- (iNa != 0 & iNa %in% ii)
-      if (Nident) id <- Inf
-      else id    <- 1/sqrt(min(eigen(t(S)%*%S)$value))
-      psub     <- rep(0,npar)
+      if (Nident) {
+        id <- Inf
+      } else {
+        id  <- 1/sqrt(min(eigen(t(S) %*% S)$value))
+      }
+      psub     <- rep(0, npar)
       psub[ii] <- 1
       n        <- ncol(cc)
-      Collin   <<- rbind(Collin,c(psub,n,id))
+      Collin   <<- rbind(Collin, c(psub, n, id))
     }
   }
   if (is.null(parset)) {
 
     combin <- function(n, v) { # combinations of n elements from a vector p (length (p) > = n)
-
       if (n == 1)
-        matrix(data=v, ncol=1)
+        matrix(data = v, ncol = 1)
       else if (n >= length(v))
-        matrix(data=v, nrow=1)
+        matrix(data = v, nrow = 1)
       else
-        rbind(cbind(v[1],combin(n-1,v[-1])),combin(n,v[-1]))
+        rbind(cbind(v[1], combin(n-1, v[-1])), combin(n, v[-1]))
     }
 
     pset   <- 1:npar
-    if (is.null(N)) nset <- 2:npar else nset <-N
+    if (is.null(N)) nset <- 2:npar else nset <- N
     for (n in nset) {
-      numcomb <- choose(npar,n)
+      numcomb <- choose(npar, n)
       if (numcomb < 5000) {
-        cc  <-combin(n,pset)
+        cc  <- combin(n, pset)
         collFun(cc)
       }
     }
@@ -94,19 +94,19 @@ collin <- function(sensfun, parset=NULL, N=NULL, which=NULL) {
     if (is.character(parset)) {
       ln <- length(parset)
       pnames<-colnames(Sens)
-      parset <- which (pnames%in%parset)
+      parset <- which(pnames %in% parset)
       if (length(parset) != ln)
         stop ("Not all parameters in parset known")
     }
 
-    parset <- matrix(nr=1,parset)
+    parset <- matrix(nr = 1, parset)
     collFun(parset)
   }
 
   Collin <- as.data.frame(Collin)
 
-  class(Collin) <- c("collin","data.frame")
-  names(Collin) <- c(colnames(Sens),"N","collinearity")
+  class(Collin) <- c("collin", "data.frame")
+  names(Collin) <- c(colnames(Sens), "N", "collinearity")
 
   return(Collin)
 }
@@ -115,7 +115,6 @@ collin <- function(sensfun, parset=NULL, N=NULL, which=NULL) {
 ## S3 methods of collin
 ## -----------------------------------------------------------------------------
 
-## ThPe:
 plot.collin <- function(x, ...) {
 
   dots <- list(...)
@@ -124,12 +123,12 @@ plot.collin <- function(x, ...) {
   dots$main <- if(is.null(dots$main)) "Collinearity" else dots$main
 
   nc <- ncol(x)
-    do.call("stripchart",c(alist(x[,nc] ~ x[,nc-1], method="stack",
-    vertical = TRUE),dots))
+  do.call("stripchart", c(alist(x[,nc] ~ x[,nc-1], method="stack",
+    vertical = TRUE), dots))
 }
 
 
 ## -----------------------------------------------------------------------------
 
-print.collin <- function (x,...)
-  print(format(as.data.frame(unclass(x)),digits=2,scientific=FALSE,...))
+print.collin <- function (x, ...)
+  print(format(as.data.frame(unclass(x)), digits = 2, scientific = FALSE, ...))
