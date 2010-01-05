25c73951772ecec2eadd11fa17bd3c6559d9f2ca
petzoldt <petzoldt@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-01-05 23:34:27 +0000

    layout issues; example
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/fme@83 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/FME/inst/examples/modFit_with_simecol.R
+++ pkg/FME/inst/examples/modFit_with_simecol.R
@@ -12,57 +12,59 @@ yobs <- data.frame(
   S    = c(9.6, 10.2, 9.5, 8.2, 6.4, 4.9, 4.2, 3.8, 2.5, 3.8, 3.9)
 )
 
-yobs <- cbind(time=obstime, yobs)
+## Function modCost requires that simulated and observer data both
+## contain a "time" column.
+## It is already available in ysim (returned by the solver)
+## and we add it to yobs too.
+
+yobs <- cbind(time = obstime, yobs)
 
 Cost <- function(p, simObj, obstime, yobs) {
   whichpar <- names(p)
   parms(simObj)[whichpar] <- p
   times(simObj) <- obstime
   ysim <- out(sim(simObj))
-  ## modCost requires that both simulated and observer data both
-  ## contain a "time" column.
-  ## It is already available in ysim (returned by the solver)
-  ## and we add it also to yobs
-  modCost(ysim, yobs, weight="std") # same as default in fitOdeModel ??
+  modCost(ysim, yobs, weight="std")
 }
 
 ## only methods that converged without constraints in reasonable time ...
 
 system.time({
-Fit <- modFit(p = c(vm=10, km=10), f = Cost, simObj=cs1,
-  obstime=obstime, yobs=yobs, method="Nelder")
-print(summary(Fit), cov=FALSE)
-deviance(Fit)
+Fit <- modFit(p = c(vm = 10, km = 10), f = Cost, simObj = cs1,
+  obstime = obstime, yobs = yobs, method = "Nelder")
+  print(summary(Fit), cov = FALSE)
+  deviance(Fit)
 })
 
 
 system.time({
-Fit <- modFit(p = c(vm=10, km=10), f = Cost, simObj=cs1,
-  obstime=obstime, yobs=yobs, method="BFGS")
-print(summary(Fit))
-deviance(Fit)
+  Fit <- modFit(p = c(vm = 10, km = 10), f = Cost, simObj = cs1,
+    obstime = obstime, yobs = yobs, method = "BFGS")
+  print(summary(Fit))
+  deviance(Fit)
 })
 
 system.time({
-Fit <- modFit(p = c(vm=10, km=10), f = Cost, simObj=cs1,
-  obstime=obstime, yobs=yobs, method="CG")
-print(summary(Fit))
-deviance(Fit)
+  Fit <- modFit(p = c(vm = 10, km = 10), f = Cost, simObj = cs1,
+    obstime = obstime, yobs = yobs, method = "CG")
+  print(summary(Fit))
+  deviance(Fit)
 })
 
 system.time({
-Fit <- modFit(p = c(vm=10, km=10), f = Cost, simObj=cs1,
-  obstime=obstime, yobs=yobs, method="Newton")
-print(summary(Fit))
-deviance(Fit)
+  Fit <- modFit(p = c(vm = 10, km = 10), f = Cost, simObj = cs1,
+    obstime = obstime, yobs = yobs, method = "Newton")
+  print(summary(Fit))
+  deviance(Fit)
 })
 
-## the parameter fitting function for comparison
-## it is much less powerful and only a little bit faster
+## the parameter fitting function of simecol for comparison
+## it is less powerful and only a little bit faster
 system.time({
-whichpar <- c("vm", "km")
-parms(cs1)[whichpar] <- c(vm=5, km=10)
-parms(cs1)
-res <- fitOdeModel(cs1, whichpar = whichpar, obstime = obstime, yobs=yobs[-1], method="Nelder")
-res$par
+  whichpar <- c("vm", "km")
+  parms(cs1)[whichpar] <- c(vm=5, km=10)
+  parms(cs1)
+  res <- fitOdeModel(cs1, whichpar = whichpar, obstime = obstime,
+                     yobs = yobs[-1], method = "Nelder")
+  res$par
 })
