5cf638882436a4cff5b0fc05a00d13dc0e1e89f2
tomve <tomve@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-11-10 13:54:54 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/fme@114 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/FME/R/cross2long.R
+++ pkg/FME/R/cross2long.R
@@ -8,68 +8,84 @@
 ## . replicate : a character vector containing the names of columns that have to be replicated for every column name (e.g. variable name)
 ## . error     : boolean indicating whether the final dataset in long format should contain an extra column for error values (standard deviation, standard error, ...)
 
-cross2long <- function(data,
+
+cross2long <- function(data, 
                        x,
-                       include=NULL,
-                       exclude=NULL,
+                       select=NULL,
                        replicate=NULL,
-                       error=F
+                       drop=FALSE,
+                       error = FALSE,
+                       na.rm = FALSE
                       )
 {
   # Exception/error handling
-  data <- as.data.frame(data)
-  nbUnique <- length(unique(c(include,exclude,replicate)))
-  nbTotal  <- (length(include) + length(exclude) + length(replicate))
-  if(nbUnique != nbTotal)
-    stop("Include, exclude and replicate should be disjoint sets of column names")
-  if(is.null(x))
-    stop("An independent variable should be given")
-  indep <- data[,x]
-  data[x] <- NULL
-  if(!is.null(replicate)) {
-    toBeReplicated <- subset(data,select=replicate)
+  
+  varlist <- seq_along(data)
+  names(varlist) <- names(data)
+
+  if(!is.null(substitute(x))) {
+    indep <- varlist[names(varlist) %in% as.character(substitute(x))]
+    indepData <- as.data.frame(data[, indep])
+    names(indepData) <- names(varlist)[indep]
   }
-  if(!is.null(exclude))
-    data <- data[!(names(data) %in% exclude)]
-  if(!is.null(include))
-    data <- data[include]
+  else stop("no independent variable defined")
+
+  if(!is.null(substitute(select))) {
+        vars <- varlist[names(varlist) %in% as.character(substitute(select))]
+        vars <- vars[vars != indep]
+    }
+  else vars <- TRUE
+  selectedData <- as.data.frame(data[, vars])
+  names(selectedData) <- names(varlist)[vars]
+
+  if(!is.null(substitute(replicate))) { 
+    reps <- varlist[names(varlist) %in% as.character(substitute(replicate))]
+    reps <- reps[reps != indep]
+    replicatedData <- as.data.frame(data[, reps])
+    names(replicatedData) <- names(varlist)[reps]
+  }
+
 
   # gathering of data
-  stackedData <- stack(data)
-  nNames <- length(unique(stackedData$ind))
+  nVars <- ncol(selectedData)
+  stackedData <- stack(selectedData)
   nObs   <- nrow(stackedData)
-  indep <- rep(indep,times=nNames)
-  if(!is.null(replicate)) {
-    replicated <- as.data.frame(lapply(as.list(toBeReplicated), FUN=function(x) rep(x,times=nNames)))
+
+  indepData <- as.data.frame(lapply(indepData, FUN=function(x) rep(x,times=nVars)))
+
+  if(!is.null(substitute(replicate))) {
+    replication <- as.data.frame(lapply(as.list(replicatedData), FUN=function(x) rep(x,times=nVars)))
     # Construction of appropriate data structure
     if(error)
       out <- data.frame(name = factor(stackedData$ind,exclude=NULL),
-                        x    = indep,
+                        x    = indepData[[1]],
                         y    = stackedData$values,
                         err  = rep(1,nObs),
-                        as.data.frame(lapply(replicated,function(x) factor(x,exclude=NULL)))
+                        as.data.frame(lapply(replication,function(x) factor(x,exclude=NULL)))
                        )
     else
       out <- data.frame(name = factor(stackedData$ind,exclude=NULL),
-                        x    = indep,
+                        x    = indepData[[1]],
                         y    = stackedData$values,
-                        as.data.frame(lapply(replicated,function(x) factor(x,exclude=NULL)))
+                        as.data.frame(lapply(replication,function(x) factor(x,exclude=NULL)))
                        )
   }
   else {
     if(error)
       out <- data.frame(name = factor(stackedData$ind,exclude=NULL),
-                        x    = indep,
+                        x    = indepData[[1]],
                         y    = stackedData$values,
                         err  = rep(1,nObs)
                        )
     else
       out <- data.frame(name = factor(stackedData$ind,exclude=NULL),
-                        x    = indep,
+                        x    = indepData[[1]],
                         y    = stackedData$values
                        )
 
   }
+
+  if(na.rm) out <- droplevels(out[complete.cases(out),])
 
   return(out)
 }
--- pkg/FME/R/gaussianWeights.R
+++ pkg/FME/R/gaussianWeights.R
@@ -10,31 +10,58 @@
 ## weight      : scaling factor of modCost function (sd, mean, or none)
 
 gaussianWeights <- function(obs,                            # data set in long format as is typically used in modCost (e.g. Meso1 in long format)
-                            x           = "x",              # name of the independent variable (e.g. "day" in Meso1)
-                            y           = "y",
-                            xmodel      = NULL,             # unique times at which model output is produced
-                            spread      = 1/24,             # standard deviation used to calculate the weights from a normal distribution (stdev = standard deviation of that normal distribution)
+                            x = x,                          # name of the independent variable (e.g. "day" in Meso1)
+                            y = y,
+                            xmodel,                         # unique times at which model output is produced
+                            spread,                         # standard deviation used to calculate the weights from a normal distribution (stdev = standard deviation of that normal distribution)
                             weight      = "none",           # scaling factor of modCost function (sd, mean, or none)
-                            aggregation = "name",
-                            ordering    = c(aggregation,x)  # list of variables in obs used for grouping during scale factor calculation (cf. tapply)
+                            aggregation = name,
+                            ordering                        # list of variables in obs used for grouping during scale factor calculation (cf. tapply)
                            )
 {
 
+  varlist <- seq_along(names(obs))
+  names(varlist) <- names(obs)
+
+  xname <- as.character(substitute(x))
+  yname <- as.character(substitute(y))
+  aggname <- as.character(substitute(aggregation))
+  if(missing(ordering)) orderingnames <- c(aggname,xname)
+  else orderingnames <- as.character(substitute(ordering))
+  if(!(aggname %in% orderingnames)) orderingnames <- c(aggname,orderingnames)
+  if(!(xname %in% orderingnames))  orderingnames <- c(orderingnames,xname)
+
   # Exception/error handling
-  if(is.null(xmodel))
-    stop("Need times of model run to determine relevant time points")
-  if(!(x %in% names(obs)))
-    stop("Independent variable in observations not known")
+  if(!(xname %in% names(obs)))
+    stop("Independent variable in observational dataset not known")
+  if(!(yname %in% names(obs)))
+    stop("Dependent variable in observational dataset not known")
+  if(!(aggname %in% names(obs)))
+    stop("Aggregation factor unknown")
+  if(length(union(orderingnames,names(varlist))) > length(varlist))
+    stop("At least one ordering variable not defined in observations")
+
+  xind <- varlist[names(varlist) == substitute(x)]
+  yind <- varlist[names(varlist) == substitute(y)]
+  aggind <- varlist[names(varlist) == substitute(aggregation)]
+
+
+  if(missing(xmodel)) {
+    warning("No independent variable for output specified: using independent variable of input instead")
+    xmodel <- unique(unlist(obs[,xind]))
+  }
+
   if(!(weight %in% c("mean","sd","none")))
-    stop("Unknown weighing")
+    stop("Unknown weighing factor")
 
   # Selection of indices of relevant model times
-  obsTimesSelect <- sapply(unique(obs[,x]),
+  obsTimesSelect <- lapply(as.list(unique(obs[,xind])),
                            FUN = function(t) which(abs(xmodel - t) == 0)
                            )
-  obsRangeSelect <- sapply(unique(obs[,x]),
+  obsRangeSelect <- lapply(as.list(unique(obs[,xind])),
                            FUN = function(t) which(abs(xmodel - t) <= 3*spread)
                            )
+
   # Collection of actual model times
   obsTimes   <- lapply(obsTimesSelect, function(t) xmodel[t])
   obsRanges  <- lapply(obsRangeSelect, function(t) xmodel[t])
@@ -53,14 +80,14 @@ gaussianWeights <- function(obs,                            # data set in long f
                        xmodel=unlist(obsRanges)
                       )
   # Merging with observations to obtain an extended observational dataset
-  fullObs <- merge(obs,errors,by.x=x,by.y="time",all.y=T)
-  fullObs[,x] <- fullObs[,"xmodel"]   # observational times are replaced by model times
-  fullObs[,"xmodel"] <- NULL          # the latter column is redundant, hence removed
+  fullObs <- merge(obs,errors,by.x=xind,by.y="time",all.y=T)
+  fullObs[,xname] <- fullObs[,"xmodel"]   # observational times are replaced by model times
+  fullObs[,"xmodel"] <- NULL                             # the latter column is redundant, hence removed
 
   if(weight != "none") {
-    aggregationFunction <- tapply(fullObs[,y],INDEX=fullObs[,rev(aggregation)],weight,na.rm=T)
+    aggregationFunction <- tapply(fullObs[,yname],INDEX=fullObs[,rev(aggname)],weight,na.rm=T)
     factorLevels <- dimnames(aggregationFunction)
-    names(factorLevels) <- rev(aggregation)
+    names(factorLevels) <- rev(aggname)
     aggregationFactor <- data.frame(expand.grid(factorLevels),
                                     scaling  = abs(array(aggregationFunction))
                                    )
@@ -70,14 +97,14 @@ gaussianWeights <- function(obs,                            # data set in long f
   }
 
   fullObs <- eval(parse(text=paste("fullObs[order(",
-                                   paste("fullObs$",ordering,sep="",collapse=","),
+                                   paste("fullObs$",orderingnames,sep="",collapse=","),
                                    "),]",
                                    collapse=""
                                   )
                        )
                  )
 
-  firstColumns <- c(aggregation[1],x,y,"err")
+  firstColumns <- c(aggname[1],xname,yname,"err")
   lastColumns  <- setdiff(names(fullObs),firstColumns)
   fullObs <- data.frame(fullObs[,firstColumns],fullObs[,lastColumns])
   names(fullObs) <- c(firstColumns,lastColumns)
